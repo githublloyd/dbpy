76b991e86cdd1d55b5b0740fa9220662b49099b2
Lloyd O'Brien <lloydobrien@live.ie>
2019-05-22 15:54:17 +0000

    Update README.md and update .py file to read both bus stops I use

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
-Testing Jenkins on AWS. Automatic builds after each commit. Sample.
+### Dublin Bus API & Python
 
-https://www.digitalocean.com/community/tutorials/how-to-set-up-continuous-integration-pipelines-in-jenkins-on-ubuntu-16-04
-
-https://dzone.com/articles/adding-a-github-webhook-in-your-jenkins-pipeline
-
-Test text!
+Stop 381: Leaving old house and training
+Stop 271: Leaving town near work
--- dublinbus.py
+++ dublinbus.py
@@ -3,14 +3,23 @@ import json
 import datetime
 import time
 
-url = 'https://data.smartdublin.ie/cgi-bin/rtpi/realtimebusinformation?stopid=381&routeid=1&format=json'
+url1 = 'https://data.smartdublin.ie/cgi-bin/rtpi/realtimebusinformation?stopid=381&routeid=1&format=json'
+url2 = 'https://data.smartdublin.ie/cgi-bin/rtpi/realtimebusinformation?stopid=271&routeid=1&format=json'
 
-json_obj = urllib2.urlopen(url)
+json_obj1 = urllib2.urlopen(url1)
+json_obj2 = urllib2.urlopen(url2)
 
-data = json.load(json_obj
+data1 = json.load(json_obj1)
+data2 = json.load(json_obj2)
 
-print("The date and time is " + data['timestamp'])
-print("Your stop number is " + data['stopid'])
-
-for item in data['results']:
- print("The next #1 bus is coming in (minutes) " + item['duetime'])
+### leaving Sandymount ###
+print('The date and time is ' + data1['timestamp'])
+print("\n")
+print("Your stop number is " + data1['stopid'] + " (Park Avenue, St. john's Church)")
+for item in data1['results']:
+ print("The next #1 bus is coming in " + item['duetime'] + " minutes")
+print("\n")
+### leaving O'Connell St. ###
+print("Your stop number is " + data2['stopid'] + " (O'Connell St., Abbey St.)")
+for item in data2['results']:
+ print("The next #1 bus is coming in " + item['duetime'] + " minutes")
